59e98920056e62055f77fb150096e1abe550389f
Amiro <Amiro@DESKTOP-RPRJ7SA>
2022-12-21 16:35:50 +0500

    Global.asax.cs

--- BookStore.WebUI/Global.asax.cs
+++ BookStore.WebUI/Global.asax.cs
@@ -1,5 +1,9 @@
-using BookStore.Domain.Entities;
+﻿using BookStore.Domain.Entities;
 using BookStore.WebUI.Infrastructure.Binders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
